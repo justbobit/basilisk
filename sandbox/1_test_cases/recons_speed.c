b5a585e963c5b09f2aaa276ef373fcabf2d618b6
LIMARE Alexandre <limare@blum.polytechnique.fr>
2019-09-04 15:49:47 +0200

    trying to do an advection of v_pc using the gradient of the distance. showing promising results

--- sandbox/1_test_cases/recons_speed.c
+++ sandbox/1_test_cases/recons_speed.c
@@ -1,13 +1,21 @@
+#include "run.h"
+#include "timestep.h"
+#include "bcg.h"
+
 #include "embed.h"
 #include "basic_geom.h"
 #include "view.h"
+#define MAXLEVEL 5
 
+
+double (* gradient) (double, double, double) = NULL;
 
 scalar dist[];
 face vector v_pc[];
 scalar * level_set = {dist};
 
-#define MAXLEVEL 5
+scalar * velocity = {v_pc.x,v_pc.y};
+
 int nb_cell_NB;
 
 double geometry(double x, double y, double Radius) {
@@ -53,12 +61,47 @@ int main(){
   disk. */
   foreach_face(){
     if(cs[] != 0. && cs[] != 1.){
+      v_pc.x[] = 1.;
+     }
+     else{
       v_pc.x[] = 0.;
     }
   }
   
   boundary((scalar*){v_pc});
 
+
+  face vector grad_dist[];
+  foreach(){
+
+    if(dist[]>0.){
+    foreach_dimension(){
+      double a = max(0.,dist[]    - dist[-1,0]);
+      double b = min(0.,dist[1,0] - dist[]    );
+      grad_dist.x[] = max(a,b);
+      }
+    }
+    else{
+      foreach_dimension(){
+        double a = min(0.,dist[]    - dist[-1,0]);
+        double b = max(0.,dist[1,0] - dist[]    );
+       grad_dist.x[] = max(a,b);
+      }
+    }
+  }
+
+  for (scalar f in velocity)
+    f.gradient = gradient;
+
+	
+  
+  advection (velocity, grad_dist, L0/(1 << MAXLEVEL));
+  view (fov = 20.0645, quat = {0,0,0,1}, tx = -0.501847, ty = -0.497771, 
+  	bg = {1,1,1}, width = 600, height = 600, samples = 1);
+  cells();
+	draw_vof("cs");
+  squares("v_pc.y", min =-1., max = 1.);
+  save ("vpcx.png");
 /**
 	Here we tag the cells where we will reconstruct the speed away from the
 	interface
@@ -75,6 +118,7 @@ int main(){
   			x_tag1[] = 0.;
   	}
   }
+  boundary({x_tag,x_tag1});
 
   int j;
   for (j=1;j<=nb_cell_NB;j++){
@@ -84,10 +128,13 @@ int main(){
   				x_tag1[] = 1.;
   		}
     }
+    boundary({x_tag1});
 
     foreach(){
     	x_tag[] = x_tag1[];
     }
+    boundary({x_tag});
+
   }
   view (fov = 20.0645, quat = {0,0,0,1}, tx = -0.501847, ty = -0.497771, 
   	bg = {1,1,1}, width = 600, height = 600, samples = 1);
@@ -95,78 +142,9 @@ int main(){
 	draw_vof("cs");
   squares("x_tag", min =0., max = 1.);
   save ("x_tag.png");
-  dump();
-
-// /**
-// Here we try define method to reconstruct a speed calculated in interfacial cells
-// away from the interface
-// */
-
-
-//     foreach(){
-//       // if(fabs(dist[])<0.9*NB_width){
-
-//         coord grad_dist;
-//         if(dist[]>0.){
-//         foreach_dimension(){
-//           double a = max(0.,dist[]    - dist[-1,0]);
-//           double b = min(0.,dist[1,0] - dist[]    );
-//           grad_dist.x = max(a,b);
-//           }
-//         }
-//         else{
-//           foreach_dimension(){
-//             double a = min(0.,dist[]    - dist[-1,0]);
-//             double b = max(0.,dist[1,0] - dist[]    );
-//            grad_dist.x = max(a,b);
-//           }
-//         }
-//         int k1, k2;
-//         int sig[2] = {1 , 1 };
-//         if(dist[]>0.){
-//           if(grad_dist.x>0) sig[0] = -1;
-//           if(grad_dist.y>0) sig[1] = -1;
-//         }
-//         else{
-//           if(grad_dist.x<0) sig[0] = -1;
-//           if(grad_dist.y<0) sig[1] = -1;
-//         }
-        
-//         double ratio = fabs(grad_dist.x)/(max(SEPS,fabs(grad_dist.y))) ;
-//         if(ratio > sqrt(3.)){
-//           k1 = 1;
-//           k2 = 0;
-//         }
-//         else{
-//           if(ratio > 1./sqrt(3.)){
-//             k1 = 1;
-//             k2 = 1;   
-//           }
-//         else{
-//           k1 = 0;
-//           k2 = 1; 
-//           }
-//         }
-//         v_pc2.x[] = (v_pc.x[sig[0]*k1,sig[1]*k2])
-//                     *exp( -((fabs(dist[]-grad_dist.x*Delta/2.))/
-//                       (2.*nb_cell_NB*Delta)));
-//         v_pc2.y[] = v_pc.y[sig[0]*k1,sig[1]*k2]
-//                     *exp( -((fabs(dist[]-grad_dist.y*Delta/2.))/
-//                       (2.*nb_cell_NB*Delta)));
-//       // }
-//     }
-//     boundary((scalar *){v_pc2});
-//     restriction((scalar *){v_pc2});    
-
-
-//     foreach_face(){
-//       if(v_pc.x[]==0. && v_pc2.x[] !=0.) v_pc.x[] = v_pc2.x[];
-//     }
-
-//     boundary((scalar *){v_pc});
-//     restriction((scalar *){v_pc});
-  
-
+  dump();  
 
 
 }
+
+
