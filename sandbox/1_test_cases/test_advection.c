00db3ef6b9e158386fee857a0989ab78b09b0c63
LIMARE Alexandre <limare@blum.polytechnique.fr>
2019-11-22 17:24:01 +0100

    update of the test_advection test case, faster, no gif generation

--- sandbox/1_test_cases/test_advection.c
+++ sandbox/1_test_cases/test_advection.c
@@ -12,6 +12,10 @@ scheme and the reinitialization function of the LS function. */
 #include "../level_set.h"
 #include "basic_geom.h"
 
+
+#define T 2*10*(1 << 8)
+
+
 /**
 The volume fraction is stored in scalar field `f` which is listed as
 an *interface* for the VOF solver. The level set function is a *tracer* `dist`.
@@ -31,11 +35,10 @@ the 0 value of the level set function. For this test case, the NB is made of
 only 4 cells. 
  */
 
-int     MAXLEVEL;
-int     nb_cell_NB =  6 ;  // number of cells for the NB
+int     MAXLEVEL = 8;
+int     nb_cell_NB =  8 ;  // number of cells for the NB
 double  NB_width ;              // length of the NB
 
-int     N_display = 8;          // maximum level of refinement for which we 
                                 // will display the results
 double mass_ls_init, mass_vof_init;
 
@@ -50,15 +53,12 @@ int main() {
 
   periodic(right);
   periodic(top);
-  for (MAXLEVEL = 8; MAXLEVEL <= N_display; MAXLEVEL++) {
     NB_width = L0*nb_cell_NB / (1<<MAXLEVEL);
     init_grid (1 << MAXLEVEL);
     run();
-  }
 }
 
 
-#define T 2*40*(1 << 8)
 
 
 coord center_circle ={0.,0.};
@@ -77,12 +77,12 @@ event init (i = 0) {
   fraction (f, circle(x,y,center_circle,Radius));
 
 
-  foreach_vertex(){
+  foreach(){
     dist[] = -clamp(circle(x,y,center_circle,Radius),
      -1.02*NB_width, 1.02*NB_width);
   }
 
-  if (N == (1<<N_display)) {
+  if (N == (1<<MAXLEVEL)) {
     scalar l[];
     // foreach()
     //   l[] = level;
@@ -113,20 +113,19 @@ zero and one. */
 event logfile (t = {0,T}) {
   stats s = statsf (f);
   fprintf (stderr, "# %f %.12f %.9f %g\n", t, s.sum, s.min, s.max);
-  if(N == (1<<N_display)) {
+  if(N == (1<<MAXLEVEL)) {
     mass_vof_init = s.sum;
   }
   scalar f2[];
   fractions (dist, f2);
   s = statsf (f2);
   fprintf (stderr, "# %f %.12f %.9f %g\n", t, s.sum, s.min, s.max);
-  if(N == (1<<N_display)) {
+  if(N == (1<<MAXLEVEL)) {
     mass_ls_init = s.sum;
   }
-  if (N == (1<<N_display))
+  if (N == (1<<MAXLEVEL))
   output_facets (f);
 }
-
 
 /**
 To compute the errors, we reinitialise field `e` at the end of the
@@ -161,68 +160,25 @@ event field (t = T) {
 }
 
 /**
-We make simple movies showing the field `f`, the cells used for the NB and the
-level set function during the simulation.
-*/
-
-event levels2 (t += T/300){
-  if (N == (1<<N_display)) {
-  
-    scalar l[];
-    foreach()
-      l[] = f[];
-    output_ppm (l, file = "f_reversed2.gif", n = 400, 
-      opt = "--delay 1",min = 0, max = 1);
-
-    foreach()
-      l[] = dist[];
-    output_ppm (l, file = "dist2.gif", n = 400, 
-      opt = "--delay 1",min = -1.*NB_width, max=1.*NB_width);
-  }
-}
-
-/**
 Level set reinitialization event. The number of iteration of the LS_reinit2 
 function is set to  $1.4 \times 2 \times nb_{cell NB}$ which is a bit more than 
 the $NB_{width}$
 */
-
 event LS_reinitialization(i++,last){
-
-  if(i>0){
-    LS_reinit2(dist,L0/(1 << MAXLEVEL), NB_width,
-      (nb_cell_NB));
+  if(i>0 && i%2==1){
+    LS_reinit2(dist,L0/(1 << MAXLEVEL), 1.4*NB_width,
+      4);
   }
 }
 
 
-#if 0
-event movie (i += 10)
-{
-  scalar l[];
-  foreach()
-    l[] = level;
-  output_ppm (l, 1 << MAXLEVEL, file = "level.mp4");
-}
-#endif
-
 /**
 ## Results
 
-~~~gnuplot Shapes of the interface
-reset
-set size ratio -1
-plot [-0.5:0.5][-0.5:0.5]'out' w l t "adaptive"
-~~~
-
-![Animation of the level set function](test_advection/dist2.gif)(width="800" height="600") 
-
-![Animation of the VOF function](test_advection/f_reversed2.gif)(width="800" height="600") 
-
-
-Initial Level set field    |  Final Level Set field
+|                          |                          |
 :-------------------------:|:-------------------------:
-![Initial](test_advection/dist_init.png)(width="400" height="300") |  ![Final](test_advection/dist_final.png)(width="400" height="300")  
-:-------------------------:|:-------------------------:
-![Initial](test_advection/f_init.png)(width="400" height="300") |  ![Final](test_advection/f_reversed.png)(width="400" height="300")  
+| Initial Level Set        |  Final Level set         |
+|![Initial](test_advection/dist_init.png)(width="400" height="300") |  ![Final](test_advection/dist_final.png)(width="400" height="300")  |
+| Initial VOF              |  Final VOF               |
+| ![Initial](test_advection/f_init.png)(width="400" height="300") |  ![Final](test_advection/f_reversed.png)(width="400" height="300")  |
 */
